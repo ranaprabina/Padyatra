05446c710f578d77cbabc2e078229bd7690eb92c
kusalmagar <kusal.mgr33@gmail.com>
2020-07-25 20:14:31 +0545

    added presenter file for user interested route

---
+++ lib/presenter/user_interest_route_presenter.dart
@@ -0,0 +1,23 @@
+import 'package:padyatra/dependency_injector/dependency_injection.dart';
+import 'package:padyatra/models/select_user_interet_routes/user_interest_route_data.dart';
+
+abstract class UserInterestRouteListViewContract {
+  void onLoadUserInterestRouteComplete(List<UserInterestRoute> items);
+  void onLoadUserInterstRouteError();
+}
+
+class UserInterestRouteListPresenter {
+  UserInterestRouteListViewContract _viewContract;
+  UserInterestRouteRepository _repository;
+
+  UserInterestRouteListPresenter(this._viewContract) {
+    _repository = new Injector().userInterestRouteRepository;
+  }
+
+  void loadUserRoutes() {
+    _repository
+        .fetchRoutes()
+        .then((ur) => _viewContract.onLoadUserInterestRouteComplete(ur))
+        .catchError((onError) => _viewContract.onLoadUserInterstRouteError());
+  }
+}
