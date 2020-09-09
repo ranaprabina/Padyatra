8fc5bdeb34234fb9d4fcf7c6143b6cf627d7ed42
kusalmagar <kusal.mgr33@gmail.com>
2020-09-09 18:28:05 +0545

    added search functionality to search form server

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Padyatra',
+      debugShowCheckedModeBanner: false,
       home: ExplorePage(),
       routes: {},
     );
