c478819876de86167a46327cfa8602b8535b7410
kusalmagar <kusal.mgr33@gmail.com>
2020-09-09 18:32:34 +0545

    added search functionality to search from server

--- lib/view/SearchBar.dart
+++ lib/view/SearchBar.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:padyatra/models/search_route_model/search_route_data.dart';
+import 'package:padyatra/presenter/search_route_presenter.dart';
 
 class SearchBar extends StatefulWidget {
   @override
   _SearchBarState createState() => _SearchBarState();
 }
 
-class _SearchBarState extends State<SearchBar> {
-  // TextEditingController editingController = TextEditingController();
+class _SearchBarState extends State<SearchBar>
+    implements SearchRouteListViewContract {
+  SearchRouteListPresenter _presenter;
+  List<SearchRoute> _searchRoute;
+  bool _isLoading;
+  List routeNameList = [];
+  String searchingRoute;
+  _SearchBarState() {
+    _presenter = new SearchRouteListPresenter(this);
+  }
+  @override
+  void initState() {
+    super.initState();
+    _isLoading = true;
+    _presenter.loadRoutesName();
+  }
+
   Widget build(BuildContext context) {
     //list of items in the list view
     // final List<String> items = <String>['A', 'B', 'c', 'D', 'E'];
@@ -22,63 +39,83 @@ class _SearchBarState extends State<SearchBar> {
           child: Padding(
             padding: const EdgeInsets.all(8.0),
             child: TextField(
-              onChanged: (value) {},
-              // controller: editingController,
               decoration: InputDecoration(
-                  fillColor: Colors.grey.shade200,
-                  filled: true,
-                  contentPadding: EdgeInsets.all(1),
-                  labelText: "Enter Trekking Route or Trial",
-                  hintText: "Search",
-                  suffixIcon: Padding(
-                    padding: const EdgeInsetsDirectional.only(end: 0),
-                    child: IconButton(
-                      icon: Icon(Icons.search),
-                      color: Colors.black,
-                      onPressed: () {
-                        print("Search Button clicked");
-                        showSearch(context: context, delegate: RouteSearch());
-                      },
-                    ),
+                fillColor: Colors.grey.shade200,
+                filled: true,
+                contentPadding: EdgeInsets.all(1),
+                // labelText: "Enter Trekking Route or Trial",
+                hintText: "Enter Trekking Route or Trial",
+                suffixIcon: Padding(
+                  padding: const EdgeInsetsDirectional.only(end: 0),
+                  child: IconButton(
+                    icon: Icon(Icons.search),
+                    color: Colors.black,
+                    onPressed: () {
+                      print("Search Button clicked");
+                    },
                   ),
-                  border: OutlineInputBorder(
-                      borderSide: BorderSide.none,
-                      borderRadius: BorderRadius.all(Radius.circular(10.0)))),
+                ),
+                border: OutlineInputBorder(
+                  borderSide: BorderSide.none,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10.0),
+                  ),
+                ),
+              ),
+              onTap: () {
+                setState(() {
+                  print("Search box tapped");
+                  showSearch(
+                    context: context,
+                    delegate: RouteSearch(
+                      recentRouteList: routeNameList,
+                    ),
+                  );
+                });
+              },
+              readOnly: true,
             ),
           ),
         ),
-
-        // Container containing Text 'You might like this'
-        // Container(
-        //   width: double.infinity,
-        //   margin: EdgeInsets.fromLTRB(12, 10, 0, 0),
-        //   child: Text(
-        //     'You might like this',
-        //     textAlign: TextAlign.start,
-        //     style: TextStyle(
-        //       fontFamily: 'Roboto',
-        //       fontWeight: FontWeight.w400,
-        //       fontSize: 22,
-        //     ),
-        //   ),
-        // ),
       ],
     );
+  }
+
+  @override
+  void onLoadSearchRouteComplete(List<SearchRoute> items) {
+    // TODO: implement onLoadSearchRouteComplete
+    setState(() {
+      _searchRoute = items;
+      _isLoading = false;
+      print("Routes to be searched are");
+      for (int i = 0; i < _searchRoute.length; i++) {
+        final SearchRoute routesName = _searchRoute[i];
+        // print(_searchRoute[i]);
+        print(routesName.routeName);
+        routeNameList.add(routesName.routeName.toLowerCase());
+      }
+    });
+  }
+
+  @override
+  void onLoadSeachRouteError() {
+    // TODO: implement onLoadSeachRouteError
   }
 }
 
 class RouteSearch extends SearchDelegate<String> {
-  final routes = [
-    "Annapurna Base Camp Trek",
-    "Mardi Himal Trek",
-    "Poon Hill Trek",
-    "Manaslu Circuit",
-  ];
-  final recentRoutes = [
+  List routes = [];
+  List recentRoutes = [
     "Annapurna Base Camp Trek",
     "Mardi Himal Trek",
     "Poon Hill Trek",
   ];
+
+  String searchingRoute;
+  RouteSearch({List recentRouteList}) {
+    routes = recentRouteList;
+  }
+
   @override
   List<Widget> buildActions(BuildContext context) {
     // actions for app bar
@@ -113,7 +150,7 @@ class RouteSearch extends SearchDelegate<String> {
     // show some results base on the selection
     // throw UnimplementedError();
     return Card(
-      color: Colors.teal,
+      color: Colors.amber,
       // shape: StadiumBorder(),
       child: Center(
         child: Text(query),
@@ -131,28 +168,31 @@ class RouteSearch extends SearchDelegate<String> {
 
     return ListView.builder(
       itemCount: suggestionList.length,
-      itemBuilder: (context, index) => ListTile(
-        onTap: () {
-          showResults(context);
-        },
-        leading: Icon(Icons.directions_walk),
-        title: RichText(
-          text: TextSpan(
-              text: suggestionList[index].substring(0, query.length),
-              style: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-              ),
-              children: [
-                TextSpan(
-                  text: suggestionList[index].substring(query.length),
-                  style: TextStyle(
-                    color: Colors.grey,
-                  ),
+      itemBuilder: (context, index) {
+        return ListTile(
+          onTap: () {
+            showResults(context);
+            print("onTap Tapped");
+          },
+          leading: Icon(Icons.directions_walk),
+          title: RichText(
+            text: TextSpan(
+                text: suggestionList[index].substring(0, query.length),
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
                 ),
-              ]),
-        ),
-      ),
+                children: [
+                  TextSpan(
+                    text: suggestionList[index].substring(query.length),
+                    style: TextStyle(
+                      color: Colors.grey,
+                    ),
+                  ),
+                ]),
+          ),
+        );
+      },
     );
   }
 }
