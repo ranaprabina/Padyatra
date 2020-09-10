89c1eda35a8cdddb07847546b02863bc5aed8d4b
kusalmagar <kusal.mgr33@gmail.com>
2020-09-10 21:00:48 +0545

    updated API url

--- lib/models/recently_added_routes/recently_added_route_prod.dart
+++ lib/models/recently_added_routes/recently_added_route_prod.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart' as http;
 
 class ProdRecentlyAddedRouteRepository implements RecentlyAddedRouteRepository {
   String recentlyAddedRouteURL =
-      "http://192.168.1.68:8888/Padyatra/PHP%20codes/API's/recentlyAddedRoutes.php";
+      "http://192.168.1.68:8888/Padyatra/PHP%20codes/Padyatra-ServerSide/API's/recentlyAddedRoutes.php";
   @override
   Future<List<RecentlyAddedRoute>> fetchRecentRoutes() async {
     http.Response response = await http.get(recentlyAddedRouteURL);
