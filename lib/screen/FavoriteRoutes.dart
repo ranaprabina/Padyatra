5a709f636923dfba41ca7f4fb13b33f7bc31b463
ranaprabina <ranaprabina953@gmail.com>
2020-09-15 13:17:12 +0545

    This is the first commit

---
+++ lib/screen/FavoriteRoutes.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FavoriteRoutes extends StatefulWidget {
+  @override
+  _FavoriteRoutesState createState() => _FavoriteRoutesState();
+}
+
+class _FavoriteRoutesState extends State<FavoriteRoutes> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
