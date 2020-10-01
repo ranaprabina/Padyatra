25f75e9f3e86a81f869189315e2d26fcf16d3c21
ranaprabina <ranaprabina953@gmail.com>
2020-10-01 18:22:32 +0545

    minor change

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:padyatra/dependency_injector/dependency_injection.dart';
 
-import 'package:padyatra/screen/OnBoardingScreen.dart';
 import 'package:padyatra/screen/SplashScreen.dart';
 
 void main() {
