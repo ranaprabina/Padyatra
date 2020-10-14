26a8996cd4427d3ae1158c11cec390cbad9ea1b5
ranaprabina <ranaprabina953@gmail.com>
2020-10-14 10:31:07 +0545

    check user login

--- lib/Login.dart
+++ lib/Login.dart
@@ -1,12 +1,68 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:padyatra/control_sizes.dart';
-import 'package:padyatra/screen/HomePage.dart';
-import 'package:padyatra/screen/SignUp.dart';
+import 'package:http/http.dart' as http;
 
-class Login extends StatelessWidget {
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:padyatra/screen/ProfilePage.dart';
+
+class Login extends StatefulWidget {
   const Login({
     Key key,
   }) : super(key: key);
+
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  TextEditingController email = new TextEditingController();
+  TextEditingController password = new TextEditingController();
+  String u_id;
+  // TextEditingController u_id = TextEditingController();
+  // final _formKey = new GlobalKey<FormState>();
+  // String u_id = u_idController.text;
+  Future login() async {
+    final response = await http.post(
+        // "http://192.168.1.65/PHP%20codes/Padyatra/API's/inserUserData.php",
+        "http://192.168.1.65/PHP%20codes/Padyatra/API's/login.php",
+        body: {
+          "email": email.text,
+          "password": password.text,
+          // "u_id": u_id.text,
+        });
+    var data = json.decode(response.body);
+
+    print(data);
+    print(data.length);
+    var data1 = json.decode(data['serverResponse']);
+    print(data1.length);
+    // print(data['User Id']);
+    // u_id = data['User Id'];
+    // if (data['Respone'] == "login_success") {
+    //   // await FlutterSession().set('token', email.text);
+    //   Fluttertoast.showToast(
+    //       msg: "Login Sucessful",
+    //       toastLength: Toast.LENGTH_SHORT,
+    //       gravity: ToastGravity.CENTER,
+    //       // timeInSecForIosWeb: 1,
+    //       backgroundColor: Colors.grey,
+    //       textColor: Colors.white,
+    //       fontSize: 16.0);
+    //   Navigator.of(context).push(
+    //       MaterialPageRoute(builder: (context) => ProfilePage(u_id: u_id)));
+    // } else {
+    //   Fluttertoast.showToast(
+    //       msg: "Username and password Incorrect",
+    //       toastLength: Toast.LENGTH_SHORT,
+    //       gravity: ToastGravity.CENTER,
+    //       // timeInSecForIosWeb: 1,
+    //       backgroundColor: Colors.grey,
+    //       textColor: Colors.white,
+    //       fontSize: 16.0);
+    // }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -56,42 +112,50 @@ class Login extends StatelessWidget {
               borderRadius: BorderRadius.circular(10.0),
               color: Colors.transparent,
             ),
-            child: Column(
-              children: <Widget>[
-                Container(
-                  padding: EdgeInsets.all(10.0),
-                  decoration: BoxDecoration(
-                    border: Border(
-                      bottom: BorderSide(
-                        color: Colors.grey[100],
+            child: Form(
+              // key: _formKey,
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.all(10.0),
+                    decoration: BoxDecoration(
+                      border: Border(
+                        bottom: BorderSide(
+                          color: Colors.grey[100],
+                        ),
                       ),
                     ),
-                  ),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                      hintText: "Username",
-                      hintStyle: TextStyle(color: Colors.grey),
-                    ),
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.all(10.0),
-                  decoration: BoxDecoration(
-                    border: Border(
-                      bottom: BorderSide(
-                        color: Colors.grey[100],
-                      ),
-                    ),
-                  ),
-                  child: TextField(
-                    decoration: InputDecoration(
+                    child: TextFormField(
+                      controller: email,
+                      // validator: EmailValidator(
+                      //     errorText: 'enter a valid email address'),
+                      decoration: InputDecoration(
                         border: InputBorder.none,
-                        hintText: "Password",
-                        hintStyle: TextStyle(color: Colors.grey)),
+                        hintText: "Email",
+                        hintStyle: TextStyle(color: Colors.grey),
+                      ),
+                    ),
                   ),
-                )
-              ],
+                  Container(
+                    padding: EdgeInsets.all(10.0),
+                    decoration: BoxDecoration(
+                      border: Border(
+                        bottom: BorderSide(
+                          color: Colors.grey[100],
+                        ),
+                      ),
+                    ),
+                    child: TextFormField(
+                      obscureText: true,
+                      controller: password,
+                      decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "Password",
+                          hintStyle: TextStyle(color: Colors.grey)),
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
           SizedBox(
@@ -112,8 +176,7 @@ class Login extends StatelessWidget {
           ),
           GestureDetector(
             onTap: () {
-              Navigator.of(context)
-                  .push(MaterialPageRoute(builder: (context) => HomePage()));
+              login();
             },
             child: Container(
               height: displayHeight(context) * 0.06,
@@ -136,8 +199,7 @@ class Login extends StatelessWidget {
           ),
           GestureDetector(
             onTap: () {
-              Navigator.of(context)
-                  .push(MaterialPageRoute(builder: (context) => SignUp()));
+              print('doafgh');
             },
             child: Container(
               child: Center(
