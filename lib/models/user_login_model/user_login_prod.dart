921b6984b461602e9351afedaa98131922e7f5db
kusalmagar <kusal.mgr33@gmail.com>
2020-11-11 21:43:38 +0545

    minor changes and stores userData to shared_preferences local storage

--- lib/models/user_login_model/user_login_prod.dart
+++ lib/models/user_login_model/user_login_prod.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 import 'package:padyatra/models/user_login_model/user_login_data.dart';
 import 'package:padyatra/services/api.dart';
+import 'package:padyatra/services/sharedPreferences.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ProdUserLoginRepository implements UserLoginRepository {
   @override
@@ -12,12 +14,16 @@ class ProdUserLoginRepository implements UserLoginRepository {
     var response = await ApiCall().postData(data, 'login');
     final responseBody = jsonDecode(response.body);
     final List responseBody1 = responseBody['serverResponse'];
+    var token = responseBody['token'];
     final statusCode = response.statusCode;
 
     if (statusCode != 200 && responseBody == null && responseBody1 == null) {
       throw new FetchDataException1(
           "An Error Occured during user login process : [Status Code : $statusCode");
     }
+
+    HoldUserData().holdUserData(token, jsonEncode(responseBody1[0]));
+
     return responseBody1.map((sR) => new UserLogin.fromMap(sR)).toList();
   }
 }
