5782998b255bdc42e16b888a6313175bb9893ac1
kusalmagar <kusal.mgr33@gmail.com>
2021-02-27 14:43:34 +0545

    minor changes

--- lib/screen/HomePage.dart
+++ lib/screen/HomePage.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:hexcolor/hexcolor.dart';
 import 'package:padyatra/screen/CompletedRoutes.dart';
 import 'package:padyatra/screen/Explore.dart';
 import 'package:padyatra/screen/FavoriteRoutes.dart';
