49becd9430ea36764786a6223ee5b39b0a50e80c
kusalmagar <kusal.mgr33@gmail.com>
2021-03-06 22:19:50 +0545

    presenter file for nearby routes added

---
+++ lib/presenter/nearBy_route_presenter.dart
@@ -0,0 +1,23 @@
+import 'package:padyatra/dependency_injector/dependency_injection.dart';
+import 'package:padyatra/models/nearBy_route_model/nearBy_route_data.dart';
+
+abstract class NearByRouteListViewContract {
+  void onLoadNearByRouteComplete(List<NearByRoute> items);
+  void onLoadNearByRouteError();
+}
+
+class NearByRouteListPresenter {
+  NearByRouteListViewContract _viewContract;
+  NearByRouteRepository _nearByRouteRepository;
+
+  NearByRouteListPresenter(this._viewContract) {
+    _nearByRouteRepository = new Injector().nearByRouteRepository;
+  }
+
+  void loadNearByRoutes() {
+    _nearByRouteRepository
+        .fetchNearByRoutes()
+        .then((nr) => _viewContract.onLoadNearByRouteComplete(nr))
+        .catchError((onError) => _viewContract.onLoadNearByRouteError());
+  }
+}
