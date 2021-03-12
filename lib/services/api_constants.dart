33b12fddb61200955167ed8a609e389c37ef5956
kusalmagar <kusal.mgr33@gmail.com>
2021-03-12 18:35:32 +0545

    added URL for image

--- lib/services/api_constants.dart
+++ lib/services/api_constants.dart
@@ -1,4 +1,5 @@
 class ApiConstants {
   final String baseURL = 'http://192.168.1.68:8000/api/';
   // final String baseURL = 'http://192.168.1.65:8000/api/';
+  final String imageBaseUrl = 'http://192.168.1.68:8000/images/';
 }
