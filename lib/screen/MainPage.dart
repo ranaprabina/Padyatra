6452df37df482c52f49020a6711d0556e29d29bd
kusalmagar <kusal.mgr33@gmail.com>
2021-03-27 15:23:08 +0545

    removed unused codes

--- lib/screen/MainPage.dart
+++ lib/screen/MainPage.dart
@@ -3,8 +3,6 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'package:padyatra/Login.dart';
-import 'package:padyatra/models/get_route_coordinates_model/get_route_coordinates_data.dart';
-import 'package:padyatra/presenter/get_route_coordinates_presenter.dart';
 
 import 'package:padyatra/screen/HomePage.dart';
 import 'package:padyatra/screen/SignUp.dart';
@@ -14,38 +12,10 @@ class GuestUser extends StatefulWidget {
   _GuestUserState createState() => _GuestUserState();
 }
 
-class _GuestUserState extends State<GuestUser>
-    implements GetRouteCoordinatesListViewContract {
-  GetRouteCoordinates getRouteCoordinates;
-  GetRouteCoordinatesListPresenter _getRouteCoordinatesListPresenter;
-  List<GetRouteCoordinates> _getRouteCoordinatesServerResponse;
-  bool _isRouteIdLoading;
-
-  _GuestUserState() {
-    _getRouteCoordinatesListPresenter =
-        new GetRouteCoordinatesListPresenter(this);
-  }
-
-  void showRouteId() {
-    // routId.add(getRouteCoordinates.routeId);
-    _isRouteIdLoading
-        ? print("route is as not added into list")
-        : print(routId);
-
-    print(routId.length);
-    for (int a = 0; a < routId.length; a++) {
-      print(routId[a]);
-      // print("calling api function to retrieve nearby route data");
-    }
-
-    // print(getRouteCoordinates.routeId);
-  }
-
+class _GuestUserState extends State<GuestUser> {
   @override
   void initState() {
     super.initState();
-    _isRouteIdLoading = true;
-    // _getRouteCoordinatesListPresenter.loadServerResponseCoordinates();
   }
 
   @override
@@ -218,51 +188,5 @@ class _GuestUserState extends State<GuestUser>
         ],
       ),
     ));
-  }
-
-  double testLatitude = 28.21688560396354;
-  double testLongitude = 83.98131;
-  List routId = <String>[];
-
-  @override
-  void onGetRouteCoordinatesComplete(List<GetRouteCoordinates> items) {
-    setState(() {
-      _getRouteCoordinatesServerResponse = items;
-
-      // print(_getRouteCoordinatesServerResponse.length);
-
-      // for (var i = 0; i < _getRouteCoordinatesServerResponse.length; i++) {
-      //   getRouteCoordinates = _getRouteCoordinatesServerResponse[i];
-
-      //   print(getRouteCoordinates.coordinates.routeCoords[i]);
-
-      //   if ((getRouteCoordinates.coordinates.routeCoords[0]['lat'] %
-      //           testLatitude) <=
-      //       1) {
-      //     print("Nearby routes found");
-      //     print(getRouteCoordinates.coordinates.routeCoords[0]['lat']);
-      //     print(getRouteCoordinates.coordinates.routeCoords[0]['lat'] %
-      //         testLatitude);
-
-      //     print(100 % 20);
-      //     routId.add(_getRouteCoordinatesServerResponse[i].routeId);
-      //   } else {
-      //     print("this is not a nearby route");
-      //     print(testLatitude);
-      //     print(getRouteCoordinates.coordinates.routeCoords[0]['lat'] %
-      //         testLatitude);
-      //     print(
-      //         "far coordinates ${getRouteCoordinates.coordinates.routeCoords[0]['lat']}");
-      //   }
-      //   // }
-      // }
-
-      _isRouteIdLoading = false;
-    });
-  }
-
-  @override
-  void onGetRouteCoordinatesError() {
-    // TODO: implement onGetRouteCoordinatesError
   }
 }
