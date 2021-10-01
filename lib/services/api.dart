3374f042951dbe04bc59ca68ee7005e9281cfb13
kusalmagar <kusal.mgr33@gmail.com>
2021-10-01 20:56:34 +0545

    updated for multiform post

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -20,21 +20,7 @@ class ApiCall {
     request.files.add(await http.MultipartFile.fromPath(
         'profile_photo_path', profilePhotoPath));
     request.fields['remember_token'] = rememberToken;
-    // var res = await request.send();
-    // final statusCode = res.statusCode;
-    // print("res statusCode is");
-    // print(res.statusCode);
-    // print(res.reasonPhrase);
     return await request.send();
-    // return await http.post(fullUrl,
-    //     headers: {'Content-type': "multipart/form-data"},
-    //     body: jsonEncode(data),
-    //     //     {
-    //     //   "lang": "fas",
-    //     //   "image": data["profile_photo_path"],
-    //     //   "remember_token": data["remember_token"]
-    //     // },
-    //     encoding: Encoding.getByName("utf-8"));
   }
 
   getData(apiUrl) async {
